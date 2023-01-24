438d9639665a28be98a634ad7ecfc7b0f2c06b20
Kaan.Arkan <Kaan.Arkan@softtech.com.tr>
2023-01-24 15:24:57 +0300

    responsive

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import Banners from "api/banners.json";
 import Title from "ui/Title";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { useWindowWidth } from "@react-hook/window-size";
 
 function NextBtn({ className, style, onClick }) {
   return (
@@ -27,17 +28,51 @@ function PrevBtn({ className, style, onClick }) {
 }
 
 function Campaigns() {
+  const windowWidth = useWindowWidth();
   const settings = {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
+    arrows: true,
     autoplaySpeed: 3000,
     cssEase: "linear",
     nextArrow: <NextBtn />,
     prevArrow: <PrevBtn />,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: false,
+        },
+      },
+    ],
   };
 
   const [banners, setBanners] = useState([]);
@@ -47,14 +82,16 @@ function Campaigns() {
   }, []);
 
   return (
-    <div className="container mx-auto py-8">
-      <Title>Kampanyalar</Title>
-      <Slider className="-mx-2" {...settings}>
+    <div className="container mx-auto md:pt-8">
+      <div className="hidden md:block">
+        <Title>Kampanyalar</Title>
+      </div>
+      <Slider className="md:-mx-2" {...settings}>
         {banners.length &&
           banners.map((banner, index) => (
             <div key={banner.id}>
-              <picture className="block px-2">
-                <img src={banner.image} className="rounded-lg" />
+              <picture className="block md:px-2">
+                <img src={banner.image} className="md:rounded-lg" />
               </picture>
             </div>
           ))}
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,7 +9,7 @@ function Cards() {
   }, []);
 
   return (
-    <div className="grid grid-cols-3 gap-x-4 ">
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 px-4 md:px-0 gap-4 ">
       {cards.length &&
         cards.map((card, index) => (
           <div
